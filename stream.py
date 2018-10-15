f150a107a5e73535749e06d5bc31450ff7df67ee
stevenhao <stevenhao@gmail.com>
2018-10-14 21:21:23 -0700

    working!

--- stream.py
+++ stream.py
@@ -5,6 +5,9 @@ import random
 import numpy as np
 import requests
 import json
+from PIL import Image, ImageDraw, ImageColor
+
+global_frame_num = 1
 
 def shuffled(lst):
     lst = lst[:]
@@ -12,12 +15,27 @@ def shuffled(lst):
     return lst
 
 def make_empty_board(width, height):
-    return [[0] * width for i in range(height)]
+    return [[None] * width for i in range(height)]
 
 def make_bag():
     return shuffled(range(7))
 
+def to_coord(i, j, img_width, img_height, board_width, board_height):
+    size = 30
+    pad_left = (img_width - board_width * 30) / 2
+    pad_top = (img_height - board_height * 30) / 2
+    return (
+            pad_left + j * size,
+            pad_top + i * size
+        )
+
+def get_color(t, active=False):
+  return ImageColor.getrgb(['cyan', 'blue', 'orange', 'yellow', 'lime', 'purple', 'red'][t])
+
+background_color = '#666666'
+
 piece_names = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
+
 class Piece:
     def __init__(self, t, s):
         self.t = t
@@ -53,16 +71,16 @@ class Game:
         for (i, j) in piece.blocks:
             self.board[i][j] = piece.t
         for i in range(self.height):
-            if all(x > 0 for x in self.board[i]):
+            if all(x != None for x in self.board[i]):
                 self.board[i: i + 1] = []
-                self.board.append([0] * width)
+                self.board = [[None] * self.width] + self.board
 
     def to_text(self, piece):
         text = ''
         blocks = piece.blocks if piece else []
         for i in range(self.height):
             for j in range(self.width):
-                if self.board[i][j] > 0:
+                if self.board[i][j] != None:
                     text += str(self.board[i][j])
                 elif (i, j) in blocks:
                     text += '+'
@@ -72,14 +90,40 @@ class Game:
         return text
 
     def to_image(self, piece, width, height):
+        global global_frame_num
         text = self.to_text(piece)
+        blocks = piece.blocks if piece else []
         print(text)
-        return np.random.rand(height, width, 3)
+        image = Image.new('RGB', (width, height))
+        draw = ImageDraw.Draw(image)
+        tc = lambda i, j: to_coord(i, j, width, height, self.width, self.height)
+        board_boundary = [tc(0, 0), tc(self.height, self.width)]
+        print(board_boundary, background_color)
+        draw.rectangle(board_boundary, background_color)
+        for i in range(self.height):
+            for j in range(self.width):
+                xy = [tc(i, j), tc(i + 1, j + 1)]
+                if self.board[i][j] != None:
+                    color = get_color(self.board[i][j])
+                    draw.rectangle(xy, color)
+                elif (i, j) in blocks:
+                    color = get_color(piece.t, True)
+                    print(xy, color)
+                    draw.rectangle(xy, color)
+        del draw
+        return image
+        #  d = global_frame_num
+        #  global_frame_num += 1
+
+        #  path = 'tmpimg%s.png' % d
+        #  image.save(open(path, 'w'))
+        #  image = Image.open(open(path, 'r'))
+        #  image.show()
 
     def to_json(self):
         data = ''
         for row in self.board:
-            data += ''.join(map(str, row))
+            data += ''.join([str(t + 1 if t != None else 0) for t in row])
         print(self.width, self.height, data)
         return {
             'board': {
@@ -125,7 +169,8 @@ class Stream:
             piece = self.path[self.path_idx]
             result = self.game.to_image(piece, self.width, self.height)
             self.path_idx += 1
-        return result
+        return np.asarray(result)
+        #  return np.random.rand(self.height, self.width, 3)
 
 
 
@@ -140,12 +185,18 @@ def main():
 
     width = 640
     height = 480
+
     stream = Stream(width, height)
+    if False:
+        for i in range(50):
+            stream.get_frame()
+        return
+
     with TwitchBufferedOutputStream(
             twitch_stream_key=args.streamkey,
             width=width,
             height=height,
-            fps=1.,
+            fps=5.,
             verbose=True) as videostream:
         while True:
             if videostream.get_video_frame_buffer_state() < 5:
